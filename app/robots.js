ab23b8ff47837f1e73463426d356cf71312cea42
imercadal <ima280@nyu.edu>
2026-03-26 09:27:28 -0400

    Eliminate profile picture from search engine crawling

--- app/robots.js
+++ app/robots.js
@@ -1,7 +1,11 @@
 export default function robots() {
   const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://drmercadal.cl';
   return {
-    rules: { userAgent: '*', allow: '/' },
+    rules: { 
+      userAgent: '*', 
+      allow: '/',
+      disallow: '/ejmercadal-azul.png'
+    },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
 }
